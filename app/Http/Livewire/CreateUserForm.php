3c780df2e104893d1d8faaab396d3450100fa7c4
David Grzyb <grzybdavid@gmail.com>
2021-04-05 15:12:02 -0400

    Add livewire component

---
+++ app/Http/Livewire/CreateUserForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CreateUserForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.create-user-form');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\CreateUserForm;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/create', CreateUserForm::class)->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
