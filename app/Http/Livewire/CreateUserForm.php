1224beeac62b8d5bc110571ce34e035dc2ed5356
David Grzyb <grzybdavid@gmail.com>
2021-04-06 13:53:47 -0400

    Complete form component

--- app/Http/Livewire/CreateUserForm.php
+++ app/Http/Livewire/CreateUserForm.php
@@ -2,10 +2,83 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class CreateUserForm extends Component
 {
+    public $currentPage = 1;
+    public $success;
+
+    // Form properties
+    public $firstName;
+    public $lastName;
+    public $email;
+    public $password;
+    public $confirmPassword;
+
+    public $pages = [
+        1 => [
+            'heading' => 'Personal Information',
+            'subheading' => 'Enter your name and email to get started.',
+        ],
+        2 => [
+            'heading' => 'Password',
+            'subheading' => 'Create a password for the new account.',
+        ],
+    ];
+
+    private $validationRules = [
+        1 => [
+            'firstName' => ['required', 'min:3'],
+            'lastName' => ['required', 'min:3'],
+            'email' => ['required', 'email', 'unique:users,email'],
+        ],
+        2 => [
+            'password' => ['required', 'string', 'min:8'],
+            'confirmPassword' => ['required', 'string', 'same:password', 'min:8'],
+        ],
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName, $this->validationRules[$this->currentPage]);
+    }
+
+    public function goToNextPage()
+    {
+        $this->validate($this->validationRules[$this->currentPage]);
+        $this->currentPage++;
+    }
+
+    public function goToPreviousPage()
+    {
+        $this->currentPage--;
+    }
+
+    public function resetSuccess()
+    {
+        $this->reset('success');
+    }
+
+    public function submit()
+    {
+        $rules = collect($this->validationRules)->collapse()->toArray();
+
+        $this->validate($rules);
+
+        User::create([
+            'name' => "{$this->firstName} {$this->lastName}",
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+        ]);
+
+        $this->reset();
+        $this->resetValidation();
+
+        $this->success = 'User created successfully!';
+    }
+
     public function render()
     {
         return view('livewire.create-user-form');
--- resources/views/livewire/create-user-form.blade.php
+++ resources/views/livewire/create-user-form.blade.php
@@ -11,65 +11,78 @@
                 <div class="md:grid md:grid-cols-3 md:gap-6">
                     <div class="md:col-span-1">
                     <div class="px-4 sm:px-0">
-                        <h3 class="text-lg font-medium leading-6 text-gray-900">Heading</h3>
-                        <p class="mt-1 text-sm text-gray-600">Subheading</p>
+                        <h3 class="text-lg font-medium leading-6 text-gray-900">{{ $pages[$currentPage]['heading'] }}</h3>
+                        <p class="mt-1 text-sm text-gray-600">{{ $pages[$currentPage]['subheading'] }}</p>
                     </div>
                     </div>
                     <div class="mt-5 md:mt-0 md:col-span-2">
-                        <div class="text-sm bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
-                            <strong class="font-bold">Oops!</strong>
-                            <span class="block sm:inline">There are some errors with your submission.</span>
-                        </div>
-                        <div class="text-sm bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
-                            <span class="block sm:inline">Success Message!</span>
-                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                                <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-                            </span>
-                        </div>
-                        <form>
+                        @if ($errors->isNotEmpty())
+                            <div class="text-sm bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+                                <strong class="font-bold">Oops!</strong>
+                                <span class="block sm:inline">There are some errors with your submission.</span>
+                            </div>
+                        @endif
+                        @if ($success)
+                            <div class="text-sm bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                                <span class="block sm:inline">{{ $success }}</span>
+                                <span wire:click="resetSuccess" class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                                    <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                                </span>
+                            </div>
+                        @endif
+                        <form wire:submit.prevent="submit">
                             <div class="shadow overflow-hidden sm:rounded-md">
                                 <div class="px-4 py-5 bg-white sm:p-6">
-                                    <div class="flex flex-col">
-                                        <div class="w-full py-2">
-                                            <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
-                                            <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                            <span class="text-xs text-red-500 mt-1">Error!</span>
+                                    @if ($currentPage === 1)
+                                        <div class="flex flex-col">
+                                            <div class="w-full py-2">
+                                                <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
+                                                <input wire:model.lazy="firstName" type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                                @error('firstName') <span class="text-xs text-red-500 mt-1">{{ $message }}</span> @enderror
+                                            </div>
+                                            <div class="w-full py-2">
+                                                <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
+                                                <input wire:model.lazy="lastName" type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                                @error('lastName') <span class="text-xs text-red-500 mt-1">{{ $message }}</span> @enderror
+                                            </div>
+                                            <div class="w-full py-2">
+                                                <label for="email_address" class="block text-sm font-medium text-gray-700">Email address</label>
+                                                <input wire:model.lazy="email" type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                                @error('email') <span class="text-xs text-red-500 mt-1">{{ $message }}</span> @enderror
+                                            </div>
                                         </div>
-                                        <div class="w-full py-2">
-                                            <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
-                                            <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                            <span class="text-xs text-red-500 mt-1">Error!</span>
+                                    @elseif ($currentPage === 2)
+                                        <div class="flex flex-col">
+                                            <div class="w-full py-2">
+                                                <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                                                <input wire:model.lazy="password" type="password" name="password" id="password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                                @error('password') <span class="text-xs text-red-500 mt-1">{{ $message }}</span> @enderror
+                                            </div>
+                                            <div class="w-full py-2">
+                                                <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
+                                                <input wire:model.lazy="confirmPassword" type="password" name="password_confirmation" id="password_confirmation" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                                @error('confirmPassword') <span class="text-xs text-red-500 mt-1">{{ $message }}</span> @enderror
+                                            </div>
                                         </div>
-                                        <div class="w-full py-2">
-                                            <label for="email_address" class="block text-sm font-medium text-gray-700">Email address</label>
-                                            <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                            <span class="text-xs text-red-500 mt-1">Error!</span>
-                                        </div>
-                                    </div>
-                                    <div class="flex flex-col">
-                                        <div class="w-full py-2">
-                                            <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                                            <input type="password" name="password" id="password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                            <span class="text-xs text-red-500 mt-1">Error!</span>
-                                        </div>
-                                        <div class="w-full py-2">
-                                            <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
-                                            <input type="password" name="password_confirmation" id="password_confirmation" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                            <span class="text-xs text-red-500 mt-1">Error!</span>
-                                        </div>
-                                    </div>
+                                    @endif
                                 </div>
                                 <div class="flex items-center justify-between px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                    <div></div>
-                                    <button type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400">
-                                        Back
-                                    </button>
-                                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        Submit
-                                    </button>
-                                    <button type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        Next
-                                    </button>
+                                    @if ($currentPage === 1)
+                                        <div></div>
+                                    @else
+                                        <button wire:click="goToPreviousPage" type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400">
+                                            Back
+                                        </button>
+                                    @endif
+                                    @if ($currentPage === count($pages))
+                                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            Submit
+                                        </button>
+                                    @else
+                                        <button wire:click="goToNextPage" type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            Next
+                                        </button>
+                                    @endif
                                 </div>
                             </div>
                         </form>
